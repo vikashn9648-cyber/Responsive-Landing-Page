3713b11c9f83d048bee6888f2c96b4960c773d4c
Vikas Nishad <vikashn9648@gmail.com>
2026-05-30 20:40:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Navbar Scroll Effect
+
+window.addEventListener("scroll", function () {
+
+    const navbar = document.getElementById("navbar");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    }
+    else {
+        navbar.classList.remove("scrolled");
+    }
+
+});
